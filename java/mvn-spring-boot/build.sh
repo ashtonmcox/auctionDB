ebfe81f1954f8d98bb605823c2ce2016bb0f348c
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-05-04 21:10:21 -0400

    Update build.sh

--- java/mvn-spring-boot/build.sh
+++ java/mvn-spring-boot/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # ITCS 3160-0002, Spring 2024
-# Marco Vieira, marco.vieira@charlotte.edu
+# ITCS 3160-0002, Spring 2024
+# Ashton Cox, ashtonmcox@outlook.com
+# Andy Pham, apham21@uncc.edu
+# Connor Schwab, cschwab3@uncc.edu
+# David Saldivar, dsaldiva@uncc.edu
+# Jamison Heinrich, jheinri2@uncc.edu
+# University of North Carolina at Charlotte
 # University of North Carolina at Charlotte
 
 
