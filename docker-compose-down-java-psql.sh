8ab23670afc1e9d28bde76a19694a5e51ddce36b
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-05-04 21:16:05 -0400

    Update docker-compose-down-java-psql.sh

--- docker-compose-down-java-psql.sh
+++ docker-compose-down-java-psql.sh
@@ -1,6 +1,10 @@
 # !/bin/bash
 # ITCS 3160-0002, Spring 2024
 # Ashton Cox, ashtonmcox@outlook.com
+# Andy Pham, apham21@uncc.edu
+# Connor Schwab, cschwab3@uncc.edu
+# David Saldivar, dsaldiva@uncc.edu
+# Jamison Heinrich, jheinri2@uncc.edu
 # University of North Carolina at Charlotte
 
 #
