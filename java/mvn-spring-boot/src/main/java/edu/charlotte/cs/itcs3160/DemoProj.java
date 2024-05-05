947a498e7d52942b886f731230b4a22fc39c543b
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-05-04 21:09:19 -0400

    Update DemoProj.java

--- java/mvn-spring-boot/src/main/java/edu/charlotte/cs/itcs3160/DemoProj.java
+++ java/mvn-spring-boot/src/main/java/edu/charlotte/cs/itcs3160/DemoProj.java
@@ -1,6 +1,10 @@
 /** **
 * ITCS 3160-0002, Spring 2024
-* Marco Vieira, marco.vieira@charlotte.edu
+* Ashton Cox, ashtonmcox@outlook.com
+* Andy Pham, apham21@uncc.edu
+* Connor Schwab, cschwab3@uncc.edu
+* David Saldivar, dsaldiva@uncc.edu
+* Jamison Heinrich, jheinri2@uncc.edu
 * University of North Carolina at Charlotte
 
 * IMPORTANT: this file includes the Python implementation of the REST API
